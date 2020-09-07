880689efa0331ce9757ccf6384ad9a36c104e35f
shuhongfan <shfshuhongfan@126.com>
2020-09-07 17:48:10 +0800

    2020.9.7 album end

--- 05album/js/album.js
+++ 05album/js/album.js
@@ -1,3 +1,17 @@
 window.onload=function (){
     document.getElementById('img_box').setAttribute('class','rotatY')
 }
+
+function img_scale(event){
+    console.log(event.target)
+    e=event.target
+    if (e.getAttribute('class')==='scale'){
+        return e.setAttribute('class','')
+    }
+    let liElements=document.getElementsByTagName('li')
+    for (let index=0;index<liElements.length;index++){
+        liElements[index].setAttribute('class','')
+    }
+    e.setAttribute('class','scale')
+}
+
