7f35e6b99160f407b23c38d353511d46608a6932
shuhongfan <shfshuhongfan@126.com>
2020-09-11 19:33:45 +0800

    2020.9.11 boot

--- 06boot/js/index.js
+++ 06boot/js/index.js
@@ -1,3 +1,10 @@
 $(function() {
     $(".flexslider").flexslider();
+    $('.box').hover(function (){
+        $(this).find('.iconfont').addClass('animated fadeInDown')
+        $(this).find('p').addClass('animated fadeInUp')
+    },function () {
+        $(this).find('.iconfont').removeClass('animated fadeInDown')
+        $(this).find('p').removeClass('animated fadeInUp')
+    })
 })
