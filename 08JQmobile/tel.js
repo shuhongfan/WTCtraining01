4b3a6dde817aefe94443a7416a071f84db2f0999
shuhongfan <shfshuhongfan@126.com>
2020-09-14 19:27:49 +0800

    2020.9.14 jqmobile

---
+++ 08JQmobile/tel.js
@@ -0,0 +1,23 @@
+let listData = [
+    {
+        "name": "张三",
+        "phone": "188888866666",
+        "address": '北京市海淀区',
+        "img": "img/tel-3.jpg",
+        "remark": "同事"
+    },
+    {
+        "name": "李四",
+        "phone": "18888887777",
+        "address": '北京市海淀区',
+        "img": "img/tel-2.jpg",
+        "remark": "同事"
+    },
+    {
+        "name": "王五",
+        "phone": "18888888888",
+        "address": '北京市海淀区',
+        "img": "img/tel-1.jpg",
+        "remark": "同事"
+    }
+]
