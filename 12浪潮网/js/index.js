83bba0ce9bb9565e7602b1dc3e819ad3c9528b97
SHUHONGFAN <shfshuhongfan@126.com>
2020-09-25 00:51:12 +0800

    2020.9.24 浪潮 end

---
+++ 12浪潮网/js/index.js
@@ -0,0 +1,16 @@
+$(function (){
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        autoplay:true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+})
